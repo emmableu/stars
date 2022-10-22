64e23d8cbd1777765b99457164c04ad5343c8c59
wwang33 <wwang33@ncsu.edu>
2022-10-22 07:40:04 -0400

    updated

--- src/markdowns/Start1230To145.jsx
+++ src/markdowns/Start1230To145.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Checkbox } from 'antd';
+import {getCondition} from "../globalConfig";
 
 
 
 const Start1230To145 = () => {
-    const onChange = (e) => {
-        console.log(`checked = ${e.target.checked}`);
-    };
     return ( <>
         <h2 id="1-submit-your-pizza-preference">1. Complete 2 design tasks</h2>
         <ul>
-            <li><p>Based on the <a href="">design requirements</a>, complete 2 design tasks.</p>
+            <li><p>Based on the
+                <a href={
+                    getCondition() ? "https://docs.google.com/document/d/16WSD7x6NItjhbIgOoU5y5sZ7HKkVOFHTzeKiFx1GEGE/edit":
+                    "https://docs.google.com/document/d/1z39azy4xbTZZyLzRRAmULSxeO7_L8WNxEl7eLrno13w/edit#"}
+                    >design requirements</a>, complete 2 design tasks.</p>
             </li>
 
             <li><p>Link to Idea Builder: <a href="https://isnap.csc.ncsu.edu/idea-builder-stars/">https://isnap.csc.ncsu.edu/idea-builder-stars/</a>.</p></li>
